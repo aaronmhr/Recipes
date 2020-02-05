fb093186f7491f9472e56debc26288ccd438d842
aaronmhr <aaronmhr@gmail.com>
2020-02-05 22:48:01 +0100

    Add app's entry point by code

--- Recipies/AppDelegate.swift
+++ Recipies/AppDelegate.swift
@@ -10,11 +10,20 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let viewController = ListRouter.assembleModule()
+        let navigationController = UINavigationController(rootViewController: viewController)
+        setRootViewController(navigationController)
         return true
     }
 }
 
+extension AppDelegate {
+    private func setRootViewController(_ viewController: UIViewController) {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = viewController
+    }
+}
