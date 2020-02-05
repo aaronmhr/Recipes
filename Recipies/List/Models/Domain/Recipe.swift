2f6aec3201d53a85d6201086a1e1a2a66083e419
aaronmhr <aaronmhr@gmail.com>
2020-02-05 22:56:39 +0100

    Add `Recipe` domain model

---
+++ Recipies/List/Models/Domain/Recipe.swift
@@ -0,0 +1,16 @@
+//
+//  Recipe.swift
+//  Recipies
+//
+//  Created by Aaron Huánuco on 05/02/2020.
+//  Copyright © 2020 Aaron Huánuco. All rights reserved.
+//
+
+import Foundation
+
+struct Recipe {
+    let title: String
+    let href: URL
+    let ingredients: String
+    let thumbnail: URL
+}
