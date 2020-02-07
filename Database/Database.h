406d15f311f434737f1b45c3a197e7fa88d0ea87
aaronmhr <aaronmhr@gmail.com>
2020-02-07 22:56:12 +0100

    Add Realm and New Database target

---
+++ Database/Database.h
@@ -0,0 +1,19 @@
+//
+//  Database.h
+//  Database
+//
+//  Created by Aaron Huánuco on 07/02/2020.
+//  Copyright © 2020 Aaron Huánuco. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Database.
+FOUNDATION_EXPORT double DatabaseVersionNumber;
+
+//! Project version string for Database.
+FOUNDATION_EXPORT const unsigned char DatabaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Database/PublicHeader.h>
+
+
--- Podfile
+++ Podfile
@@ -14,3 +14,14 @@ target 'Recipies' do
   end
 
 end
+
+target 'Database' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+  pod 'RealmSwift'
+  
+  target 'DatabaseTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+end
