3ea7ed50cfa2b06b49a505c65c8ff032522a889c
aaronmhr <aaronmhr@gmail.com>
2020-02-08 13:19:37 +0100

    Add favourite cell

---
+++ Recipies/Favorites/View/Cells/FavoriteCell.swift
@@ -0,0 +1,31 @@
+//
+//  FavoriteCell.swift
+//  Recipies
+//
+//  Created by Aaron Huánuco on 08/02/2020.
+//  Copyright © 2020 Aaron Huánuco. All rights reserved.
+//
+
+import UIKit
+import Kingfisher
+
+final class FavoriteCell: UITableViewCell, NibReusable {
+    @IBOutlet private var recipeImage: UIImageView!
+    @IBOutlet private var titleLabel: UILabel!
+    @IBOutlet private var ingredientsLabel: UILabel!
+    @IBOutlet private var hasLactoseLabel: UILabel!
+    
+    func configure(with viewModel: RecipeViewModel, delegate: FavoritesSelectorDelegate, index: Int) {
+        recipeImage.kf.setImage(with: viewModel.image, placeholder: #imageLiteral(resourceName: "placeholder"))
+        titleLabel.text = viewModel.name
+        ingredientsLabel.text = viewModel.ingredients
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        recipeImage.image = nil
+        titleLabel.text = nil
+        ingredientsLabel.text = nil
+        hasLactoseLabel.text = nil
+    }
+}
